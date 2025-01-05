51bcb630b15cf38d2bd70a91425b35560bc5a1b9
wllmz <115452984+wllmz@users.noreply.github.com>
2025-01-05 18:02:28 +0100

    update

--- go-hope-app/src/services/article/categoryService.jsx
+++ go-hope-app/src/services/article/categoryService.jsx
@@ -1,25 +1,5 @@
 import axiosInstance from "../instance/axiosInstance";
 
-<<<<<<< Updated upstream:go-hope-app/src/services/admin/categoryService.jsx
-// Créer une catégorie
-export const createCategory = async ({ category_tittle }) => {
-  try {
-    const response = await axiosInstance.post("/categories", {
-      category_tittle,
-    });
-    return response.data;
-  } catch (error) {
-    throw (
-      error.response?.data || {
-        message: "Erreur lors de la création de la catégorie.",
-      }
-    );
-  }
-};
-
-=======
->>>>>>> Stashed changes:go-hope-app/src/services/article/categoryService.jsx
-// Récupérer toutes les catégories
 export const getAllCategories = async () => {
   try {
     const response = await axiosInstance.get("/categories");
@@ -46,36 +26,3 @@ export const getCategoryById = async (categoryId) => {
     );
   }
 };
-<<<<<<< Updated upstream:go-hope-app/src/services/admin/categoryService.jsx
-
-// Mettre à jour une catégorie
-export const updateCategory = async ({ categoryId, category_tittle }) => {
-  try {
-    const response = await axiosInstance.put(`/categories/${categoryId}`, {
-      category_tittle,
-    });
-    return response.data;
-  } catch (error) {
-    throw (
-      error.response?.data || {
-        message: "Erreur lors de la mise à jour de la catégorie.",
-      }
-    );
-  }
-};
-
-// Supprimer une catégorie
-export const deleteCategory = async (categoryId) => {
-  try {
-    const response = await axiosInstance.delete(`/categories/${categoryId}`);
-    return response.data;
-  } catch (error) {
-    throw (
-      error.response?.data || {
-        message: "Erreur lors de la suppression de la catégorie.",
-      }
-    );
-  }
-};
-=======
->>>>>>> Stashed changes:go-hope-app/src/services/article/categoryService.jsx
